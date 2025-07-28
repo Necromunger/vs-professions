6c44f8276ef32bbac229393359f5b8d9c5fa18b7
Necromunger <lil_tommy_tom@hotmail.com>
2025-07-29 01:55:23 +1000

    updated clothes and command

--- Professions/ProfessionsModSystem.cs
+++ Professions/ProfessionsModSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Config;
 using Vintagestory.API.Server;
 
 namespace Professions;
@@ -18,12 +19,48 @@ public class ProfessionsModSystem : ModSystem
 
         //api.Event.ServerRunPhase(EnumServerRunPhase.WorldReady, loadCharacterClasses);
         api.Event.PlayerJoin += Event_PlayerJoin;
+
+        RegisterCommands(api);
     }
 
     private void Event_PlayerJoin(IServerPlayer byPlayer)
     {
         // Set all players to have fast walk speed the same as hunter profession
         byPlayer.Entity.Stats.Set("walkspeed", "universalfastwalk", config.WalkSpeedBuff, false);
+    }
+
+    private void RegisterCommands(ICoreServerAPI api)
+    {
+        api.ChatCommands.Create("inventory")
+            .WithDescription("Prints your worn clothing by slot")
+            .RequiresPrivilege(Privilege.chat)
+            .WithArgs(api.ChatCommands.Parsers.OnlinePlayer("target"))
+            .HandleWith(args =>
+            {
+                var player = args[0] as IServerPlayer;
+                if (player == null)
+                    return TextCommandResult.Error("Target player not found or not online.");
+
+                var invMgr = player.InventoryManager;
+                if (invMgr == null)
+                    return TextCommandResult.Error("No inventory manager available.");
+
+                var gearInv = invMgr.GetOwnInventory(GlobalConstants.characterInvClassName);
+                if (gearInv == null)
+                    return TextCommandResult.Error("Could not access worn clothing inventory.");
+
+                foreach (var slot in gearInv)
+                {
+                    if (slot.Empty)
+                        continue;
+
+                    string itemName = slot.Itemstack.Collectible.Code.ToString();
+                    player.SendMessage(GlobalConstants.GeneralChatGroup, itemName, EnumChatType.CommandSuccess);
+                }
+
+                return TextCommandResult.Success("Listed your worn clothing.");
+            });
+
     }
 }
 
